1f717e71f75ee71c6445c62362d3bbe6640c7b09
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-06-19 14:27:23 -0300

    added circleCi basic config, added a couple endpoints for books to be developed

--- src/main/java/com/gcc/coreapi/books/BooksController.java
+++ src/main/java/com/gcc/coreapi/books/BooksController.java
@@ -5,6 +5,7 @@ import com.gcc.coreapi.books.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +20,17 @@ public class BooksController {
     ResponseEntity getBooks() {
         List<Book> allBooks = bookRepository.findAll();
         return ResponseEntity.ok(allBooks);
+    }
+
+    @GetMapping("/books/id")
+    ResponseEntity getBooksById() {
+        List<Book> allBooks = bookRepository.findAll();
+        return ResponseEntity.ok(allBooks);
+    }
+
+    @PostMapping("/books")
+    ResponseEntity newBook() {
+        return ResponseEntity.ok("under development");
     }
 
 
